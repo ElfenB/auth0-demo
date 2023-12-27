6a4cb205e07e3f17ef6e693fd6cc4202d72153cb
Benjamin Elfen <benjamin@elfen-online.de>
2023-12-27 18:55:52 +0100

    feat: added some simple content

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,9 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export function Home() {
   return (
-    <Box>
-      <h1>Home</h1>
+    <Box sx={{ pt: 2 }}>
+      <Typography variant="body1">Welcome to my demo app for implementing Auth0 in a React SPA.</Typography>
     </Box>
   );
 }
